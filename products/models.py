79e6c25a90249b48cbcddbc584225c0133fb9d2c
willfs144 <willfs144@gmail.com>
2023-06-12 07:37:03 -0500

    Se crea proyecto y tabla de productos

---
+++ products/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0.0)#
+    stock = models.IntegerField()
+    create_at = models.DateTimeField(auto_now_add=True)

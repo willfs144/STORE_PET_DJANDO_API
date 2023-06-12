8a35f4f2111db47a15e323fd2fe22864c8a391fe
willfs144 <willfs144@gmail.com>
2023-06-12 13:02:54 -0500

    integramos interfaz frontend en django y archivos estaticos

--- store_pet/views.py
+++ store_pet/views.py
@@ -2,7 +2,12 @@
 from django.shortcuts import render
 
 
+from products.models import Product
+
 def index(request):
+    
+    products = Product.objects.all()
+    
     return render(request, 'index.html', {
         
     })
